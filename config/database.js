42d1a8bf0e4c3db17f5bdd316fca983413115fb5
nikolai <n.ivanov.p@gmail.com>
2016-03-04 23:38:54 +0300

    changed port for mongo

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 var config          = require('./config');
 
 module.exports = {
-    //'url' : 'mongodb://localhost:27017/test'
-    'url' : 'mongodb://'+process.env.MONGO_PORT_27017_TCP_ADDR+':'+process.env.MONGO_PORT_27017_TCP_PORT+'/test'
+    'url' : 'mongodb://mongo:27017/test'
 };
