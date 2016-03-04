361bf911d1edb0eac2d36bc78eb61779961db28f
nikolai <n.ivanov.p@gmail.com>
2016-03-04 21:59:01 +0300

    changed url to database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 var config          = require('./config');
 
 module.exports = {
-    'url' : config.get('mongoose:uri')
+    //'url' : 'mongodb://localhost:27017/test'
+    'url' : 'mongodb://'+process.env.MONGO_PORT_27017_TCP_ADDR+':'+process.env.MONGO_PORT_27017_TCP_PORT+'/test'
 };
